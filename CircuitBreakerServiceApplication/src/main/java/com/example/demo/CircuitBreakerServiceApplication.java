d70780aa4cb2059d4920f56033e1cb24fc9ecdd3
이기현 <@N14384_N1>
2017-10-20 17:46:42 +0900

    .

--- CircuitBreakerServiceApplication/src/main/java/com/example/demo/CircuitBreakerServiceApplication.java
+++ CircuitBreakerServiceApplication/src/main/java/com/example/demo/CircuitBreakerServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableCircuitBreaker
 @RestController
+@EnableEurekaClient
 public class CircuitBreakerServiceApplication {
 
 	@Autowired
